7a6bfd53eb37520a24d3e361e7da7f5fc0176bb4
Bournane Mohamed <mbournane@gmail.com>
2023-07-04 18:59:40 +0200

    display with Razor

---
+++ Controllers/ExpenditureController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuestRazor.Models;
+
+namespace QuestRazor.Controllers
+{
+    public class ExpenditureController : Controller
+    {
+        private List<Expenditure> Expenditures { get; set; }
+
+        public ExpenditureController()
+        {
+            Expenditures = new List<Expenditure>()
+            {
+                new Expenditure(1, "Energie", DateTime.UtcNow, 75.50) ,
+                new Expenditure(1, "Course", DateTime.UtcNow, 50.50),
+                new Expenditure(1, "Téléphone", DateTime.UtcNow, 35.70)
+             };
+            ViewData["Expenditures"] = Expenditures;
+
+        }
+            
+
+
+        
+        public IActionResult Index()
+        {
+            return View(Expenditures);
+        }
+    }
+}
---
+++ Models/Expenditure.cs
@@ -0,0 +1,19 @@
+﻿namespace QuestRazor.Models
+{
+    public class Expenditure
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public DateTime DateDepense { get; set;}
+        public double Price { get; set;}
+
+        public Expenditure( int id, string name, DateTime datedepense, double price)
+        {
+            Id = id;
+            Nom = name;
+            DateDepense = datedepense;
+            Price = price;
+        }
+
+    }
+}
